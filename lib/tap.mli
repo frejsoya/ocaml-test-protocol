630485e05b8e71698dd85eb41ac1fbba19c30e85
Frej Soya <frej.soya@gmail.com>
2023-10-14 15:45:04 +0200

    Initial tap14
     - parses tap as lists of inputs.

---
+++ lib/tap.mli
@@ -0,0 +1,23 @@
+type status = Ok | NotOk [@@deriving show]
+
+type tap_line =
+  | Header of string
+  | Plan of { count : int; reason : string option }
+  | Test of {
+      status : status;
+      id : int option;
+      description : string option;
+      comment : string option;
+    }
+  | Verbatim of string
+  | Bail of string
+[@@deriving show]
+
+type tap = tap_line list
+[@@deriving show]
+
+module Parser : sig
+  val tap_line : tap_line Angstrom.t
+  val tap : tap Angstrom.t
+  val read_all : string -> tap
+end
